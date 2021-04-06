b5bece7945ded0901028ce3c06185ad4072d0840
Luis Colomé <hello@luiscolome.com>
2021-04-06 11:58:03 +0200

    Add featured image.
    Add featured image to blog posts under the entry header.

--- inc/genesis-changes.php
+++ inc/genesis-changes.php
@@ -82,6 +82,24 @@ function ea_search_form() {
 }
 add_filter( 'genesis_search_form', 'ea_search_form' );
 
+/*  
+ * Display the featured image on single posts from blog.
+ * 
+ */
+function featured_post_image() {
+    if ( ! is_singular( 'post' ) )
+      return;
+      the_post_thumbnail('cfhh-featured-images');
+}
+add_action( 'genesis_entry_content', 'featured_post_image', 8 );
+
+/* 
+* Remove Genesis function to not load content images as featured image. 
+* 
+*/
+remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+remove_action( 'genesis_post_content', 'genesis_do_post_image' );
+
 
 /**
  * Disable customizer theme settings
